2969667a659eac804320f49fab6c964db40996df
Robert Dopieralski <abc507446692@gmail.com>
2019-04-09 18:56:52 +0200

    WIP: some simple code implemented

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Yo xo");
+// var time = 0;
+
+// const timer = setInterval(() => {
+//   time++;
+//   console.log(time);
+//   if (time > 5) {
+//     clearInterval(timer);
+//     timer = undefined;
+//     console.log(timer);
+//   }
+// }, 1000);
+
+console.log("dirname: ", __dirname);
+console.log("filename: " + __filename);
+
+for (i = 0; i <= 10; i++) {
+  console.log("i is equal: ", i);
+}
